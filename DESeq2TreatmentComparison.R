05231c84cab9448f7b8ab0f7e1c143aaac4ebc4c
Irem Ozkan <32915625+iremozkanRNA@users.noreply.github.com>
2026-02-27 10:25:07 -0500

    DEseq2 modified

--- DESeq2TreatmentComparison.R
+++ DESeq2TreatmentComparison.R
@@ -2,17 +2,18 @@ rm(list=ls())
 setwd("~/Desktop/")
 lapply(c("DESeq2", "apeglm"), library, character.only=T)
 
-# NDC sample YOUR CONTROL SAMPLE HERE
-sample1 <- read.csv("Manuscript/DEseq2output/FeatureCountsSplit/Processed/TetNDC_T90_BothStrand_counts.csv", header = T, row.names = "Geneid")
-# Mutant sample YOUR TREATMENT SAMPLE HERE
-sample2 <- read.csv("Manuscript/DEseq2output/FeatureCountsSplit/Processed/Tet1Q_T90_BothStrand_counts.csv", header = T, row.names = "Geneid")
+# NDC sample your CONTROL SAMPLE HERE
+sample1 <- read.table("Mutant/RNAseq/WTK/bam_output/AB/WtKChl_s2.txt", header = T, row.names = "Geneid")
+# Mutant sample your TREATMENT SAMPLE HERE
+sample2 <- read.table("Mutant/RNAseq/M1/bam_output/AB/MutantChl_s2.txt", header = T, row.names = "Geneid")
 
 #Create CountData
 countdata <- sample1[,c((ncol(sample1)-2):ncol(sample1))]
 countdata[,c(4:6)] <- sample2[,c((ncol(sample2)-2):ncol(sample2))]
+colnames(countdata) <- c("control-a", "control-b", "control-c", "treatment-a", "treatment-b", "treatment-c")
 
-colnames(countdata) <- c("wtc-a", "wtc-b", "wtc-c", "treatment-a", "treatment-b", "treatment-c")
-coldata <- read.csv("Manuscript/DEseq2output/coldata.csv", header = T, stringsAsFactors = F)
+#Read your coldata (Put control first - IMPORTANT) needs to be alphabetical
+coldata <- read.csv("Mutant/RNAseq/coldata.csv", header = T, stringsAsFactors = F)
 rownames(countdata) <- rownames(sample1)
 dds <- DESeqDataSetFromMatrix(countData = countdata,
                               colData = coldata,
@@ -25,4 +26,4 @@ res
 res <- lfcShrink(dds, coef="condition_tr_vs_ctr", type="apeglm")
 
 res
-write.csv(as.data.frame(res), file="~/Desktop/Manuscript/DEseq2output/Tet1Q_vs_NDC_t90_split.csv")
+write.csv(as.data.frame(res), file="~/Desktop/WtK_vs_M1_Chl_s2.csv")
